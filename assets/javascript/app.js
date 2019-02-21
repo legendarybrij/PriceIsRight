5fe626912b4afe91286fd74bc91b13aa1a76dca4
Brijesh Patel <Brij@Brijesh-Patels-MacBook-Pro.local>
2019-02-21 00:24:14 -0500

    same as before

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -71,7 +71,7 @@ var game = {
                         
                     }
                     curQustion++;
-                    
+                    game.start(); 
                    
                     
                 });
