d5bb2607a9d194d117ddd1e3705e320b18955f34
Brijesh Patel <Brij@Brijesh-Patels-MacBook-Pro.local>
2019-02-23 17:32:38 -0500

    mostly done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,32 +13,61 @@
 
 
 var text = $("<div>");
-var curQustion = 0;
+var curQuestion = 0;
 var correct = 0; 
-var actors = ["kate", "jane"];
-var source = "assets/images/"+actors[curQustion]+".jpg";
+var incorrect = 0;
+var unanswered =0;
+var won = 0;
+//var source = "assets/images/"+actors[curQuestion]+".jpg";
 var target = "";
+var timeMachine = 0;
+var time = 30;
 var number = 0;
 //var img = 
 //var img = "https://static2.thequizimages.com/wordpress/wp-content/uploads/2018/03/8-1.jpg?q=50&fit=crop&w=963";
-var questionOne = ["What is your name?", "Mike","Jason", "John", "Maria", "John"];
-var questionTwo = ["What is your age?", "11","30", "19", "25", "19"];
+var questionOne = ["For $100, Can you name this famous actor?", "Katherine Heigl","Jennifer Aniston", "Rachel McAdams", "Kate Winslet", "Kate Winslet",100];
+var questionTwo = ["For $200, Can you name this famous actor?", "Anthony Hawk","Christian Bale", "Tom Hanks", "Richard Gere", "Tom Hanks", 200];
+var questionThree = ["For $400, Can you name this famous actor?", "Anthony Hawk","Christian Bale", "Tom Hanks", "Richard Gere", "Tom Hanks", 400];
+var questionFour = ["For $800, Can you name this famous actor?", "Anthony Hawk","Christian Bale", "Tom Hanks", "Richard Gere", "Tom Hanks", 800];
+var questionFive = ["For $1600, Can you name this famous actor?", "Anthony Hawk","Christian Bale", "Tom Hanks", "Richard Gere", "Tom Hanks", 1600];
+var questionSix = ["For $3200, Can you name this famous actor?", "Anthony Hawk","Christian Bale", "Tom Hanks", "Richard Gere", "Tom Hanks", 3200];
+var questionSeven = ["For $6400, Can you name this famous actor?", "Anthony Hawk","Christian Bale", "Tom Hanks", "Richard Gere", "Tom Hanks", 6400];
+var questionEight = ["For $12800, Can you name this famous actor?", "Anthony Hawk","Christian Bale", "Tom Hanks", "Richard Gere", "Tom Hanks", 12800];
+var questionNine = ["For $25600, Can you name this famous actor?", "Anthony Hawk","Christian Bale", "Tom Hanks", "Richard Gere", "Tom Hanks", 25600];
+var questionTen = ["For $51200, Can you name this famous actor?", "Anthony Hawk","Christian Bale", "Tom Hanks", "Richard Gere", "Tom Hanks", 51200];
+
 var rightAudio = document.createElement("audio");
 rightAudio.setAttribute("src","assets/images/correct.mp3");
+var wrongAudio = document.createElement("audio");
+wrongAudio.setAttribute("src","assets/images/wrong.mp3");
+var timesUp = document.createElement("audio");
+timesUp.setAttribute("src","assets/images/timesup.mp3");
+
+
+
+/*var kate = document.createElement("img");
+kate.scr = "assets/images/kate.jpg";*/
+
+var kate = "assets/images/kate.jpg";
+var tom = "assets/images/tom.jpg";
+var changeImage = [kate,tom];
 
 
 var questions = [questionOne, questionTwo];
-//document.getElementById("source").innerHTML = "assets/images/kate.jpg";
+
+//document.getElementById("image").innerHTML = "assets/images/kate.jpg";
 
 $(document).ready(function(){
        $("#image").hide();
         $("#start").on("click", function(){
+            $("#question").addClass("alert alert-primary");
             $("#start").hide();
-          // document.getElementById("image").innerHTML = "assets/images/kate.jpg";
             $("#image").show();
-            game.start(); 
-            game.check();
-         
+
+                game.start(); 
+                game.check();
+           
+          
         });
                 
         
@@ -54,25 +83,52 @@ var game = {
             init: function() {
 
                 text = $("<div>");
-                curQustion = 0;
                 questionOne = ["What is your name?", "Mike","Jason", "John", "Maria", "John"];
-                questionTwo = ["What is your age?", "11","30", "19", "25"];
+                questionTwo = ["For $200, Can you name this famous actor?", "Anthony Hawk","Christian Bale", "Tom Hanks", "Richard Gere", "Tom Hanks", 200];
+                curQuestion = 0;
+                correct = 0; 
+                incorrect = 0;
+                unanswered =0;
+                won = 0;
+                //var source = "assets/images/"+actors[curQuestion]+".jpg";
+                target = "";
+                timeMachine = 0;
+                time = 30;
+                number = 0;
             
             },
             check: function(){
+                
                 $(".answer").on("click", function(event){
-                    target = event.target.id;
-                    number =target.slice(-1);
-                    if(questions[curQustion][5]===questions[curQustion][number])
-                    {
-                            rightAudio.play();
+
+                            target = event.target.id;
+                            number =target.slice(-1);
+                            if(questions[curQuestion][5]===questions[curQuestion][number])
+                            {
+                                    rightAudio.play();
+                                    correct++;
+                                    won+=questions[curQuestion][6];
+                                    $(".answer").hide();
+                                    $("#rightAnswer").removeClass("alert alert-success alert-danger alert-info");
+                                    $("#rightAnswer").show();
+                                    $("#rightAnswer").text("You got it Right. The right answer is: " + questions[curQuestion][5] +". Cheers!!" );
+                                    $("#rightAnswer").addClass("alert alert-success");
+                                    
+                            }else{
+                                wrongAudio.play();
+                                incorrect++;
+                                $(".answer").hide();
+                                $("#rightAnswer").removeClass("alert alert-success alert-danger alert-info");
+                                $("#rightAnswer").show();
+                                $("#rightAnswer").text("Ahh oh...That was incorrect. The right answer is: " + questions[curQuestion][5] );
+                                $("#rightAnswer").addClass("alert alert-danger");
+                            }
+
                             
-                    }else{
-                        
-                    }
-                    curQustion++;
-                    game.start(); 
-                   
+                                curQuestion++;
+                                setTimeout(game.start, 8500);
+                                clearInterval(timeMachine);
+                                time=30;
                     
                 });
 
@@ -81,36 +137,88 @@ var game = {
             start: function(){
                 
                     //text = $("<div>");
-                    
-                  //  $(".answer").show();
-                    $("#question").text(questions[curQustion][0]);
+                
+                if(curQuestion<questions.length)
+                {
+                    $("#image").attr("src", changeImage[curQuestion]);
+                    $(".answer").show();
+                    $("#rightAnswer").hide();
+                   document.getElementById("timeMachine").innerHTML = "Time Left: " + time;
+                   reduceTime();
+                   //initialize questions and answers
+                    $("#question").text(questions[curQuestion][0]);
                     $("#question").append(text);
 
-                    $("#answers1").text(questions[curQustion][1]);
-                  //  $("#answers1").append(text);
-                    $("#answers2").text(questions[curQustion][2]);
-                  //  text = $("<div>");
-                 //   $("#answers2").append(text);
-                    $("#answers3").text(questions[curQustion][3]);
-                 //   text = $("<div>");
-                   // $("#answers3").append(text);
-                    $("#answers4").text(questions[curQustion][4]);
+                    $("#answers1").text(questions[curQuestion][1]);
                 
+                    $("#answers2").text(questions[curQuestion][2]);
+               
+                    $("#answers3").text(questions[curQuestion][3]);
+            
+                    $("#answers4").text(questions[curQuestion][4]);
                 
-
-                
-
-                
-
+                }else
+                {
+                    game.finished();
+                }
 
 
             },
+            timesUp: function(){
+                
+                timesUp.play();
+                $(".answer").hide();
+                $("#rightAnswer").removeClass("alert alert-success alert-danger alert-info");
+                $("#rightAnswer").show();
+                $("#rightAnswer").text("Ahh oh...Your time is up. The right answer is: " + questions[curQuestion][5] );
+                $("#rightAnswer").addClass("alert alert-info");
+                    curQuestion++;
+                    unanswered++;
+                    setTimeout(game.start, 8500);
+                    clearInterval(timeMachine);
+                    time=30;
+                
+            },
+            finished:function(){
+                $("#question").hide();
+                $(".answer").hide();
+                $("#rightAnswer").hide();
+                $("#timeMachine").empty();
+                $("#image").hide();
+                $("#correct").text("Correct: "+correct);
+                $("#incorrect").text("Incorrect: "+incorrect);
+                $("#unanswered").text("Unanswered: "+unanswered);
+                if(won>0)
+                {
+                $("#money").text("Congratulations....You just have Won: $"+won);
+                }else
+                {
+                    $("#money").text("Sorry you couldn't win anything this time");
+                }
+            }
+
 
 };
 
 
   
 
-/*function set() {
-    setTimeout(function(){ }, 3000);
-  }*/
+function reduceTime() {
+    
+    timeMachine = setInterval(decrease, 1000);
+  }
+
+  function decrease(){
+    
+    if(time>0)
+      { 
+        time--;
+        document.getElementById("timeMachine").innerHTML = "Time Left: " + time;
+      }else{
+
+          game.timesUp();
+
+      }
+    
+      
+  }
